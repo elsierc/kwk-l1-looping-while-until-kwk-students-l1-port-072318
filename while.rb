658ac6a83621d3ef096b2c25be8d56fd462480d6
elsierc <elsierc22@gmail.com>
2018-07-24 22:31:15 +0000

    automatically backed up by learn

--- while.rb
+++ while.rb
@@ -1,4 +1,9 @@
 puts "What is your levitation force?"
-levitation_force = gets.strip
+levitation_force = gets.strip.to_i
 
-puts "Wingardium Leviosa!"
+puts "How much force do you need?"
+levitation_needed = gets.strip.to_i
+
+while levitation_force <= levitation_needed
+  puts "You currently have #{levitation_force} levitation force, we need more!"
+  puts "Wingardium Leviosa"
